61246997e6404a34581ff958f4aacf27aceda960
Dennis Moyseyev <denomoyseyev@gmail.com>
2018-03-26 21:22:27 -0500

    First commit

---
+++ app/src/main/java/com/example/dennismoyseyev/stopwatch/MainScreen.kt
@@ -0,0 +1,12 @@
+package com.example.dennismoyseyev.stopwatch
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainScreen : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
