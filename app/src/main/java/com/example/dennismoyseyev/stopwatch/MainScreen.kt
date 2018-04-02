49bc6df91a931cac7ab32091badacb12d09d96f6
Dennis Moyseyev <denomoyseyev@gmail.com>
2018-04-02 13:08:35 -0500

    Built out most of the data model and the functions. I need to work on the lap button. Also, add the ability to resume with the stopwatch has been stopped. I need to add this. And when reset it hit to remove everything that is store in the lap vector.

--- app/src/main/java/com/example/dennismoyseyev/stopwatch/MainScreen.kt
+++ app/src/main/java/com/example/dennismoyseyev/stopwatch/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.example.dennismoyseyev.stopwatch
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
 import android.support.v4.app.FragmentActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main_screen.*
@@ -9,12 +11,53 @@ var model: data_model= data_model()
 
 class MainScreen : FragmentActivity() {
 
+    private var mHandler = Handler()
+
+    //The function is the one that updates the time with a runnable class.
+    private val update_timer = object : Runnable {
+        override fun run() {
+            model.running_time()
+            Timer.text = model.show_time()
+            //Continues to do this without delay until stop or reset is pressed.
+            mHandler.postDelayed(this, 0)
+        }
+    }
+
+     //Function is called when the activity is created. It will set the content on the screen
+     // and bind the buttons to the correct actions.
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+        bind_button()
 
-        start_but.setOnClickListener{
-            Toast.makeText(this, "Hello world! The time is "+model.time.seconds, Toast.LENGTH_SHORT).show()
+    }
+
+    //Binds the buttons so that they can actually be used by the user.
+    private fun bind_button(){
+        start_but.setOnClickListener {
+            model.start_time=SystemClock.uptimeMillis()
+            mHandler.postDelayed(update_timer, 0)
+        }
+
+        //Displays that the stop button was pressed and it will then change the stop to say resume
+        //and when pressed it will continue from where it was paused.
+        stop_but.setOnClickListener {
+            Toast.makeText(this, "The Stopwatch was stopped.", Toast.LENGTH_SHORT).show()
+            //TODO add the ability to be able to say the time so that you can resume the stopwatch.
+            mHandler.removeCallbacks(update_timer)
+        }
+
+        //Displays the Toast that the reset button was pressed and resets the time back to zero.
+        reset_but.setOnClickListener {
+            Toast.makeText(this, "The Stopwatch was reset.", Toast.LENGTH_SHORT).show()
+            mHandler.removeCallbacks(update_timer)
+            model.reset_time()
+            Timer.text =model.show_time()
+
+            //TODO need to remove the things from the vector.
+
         }
     }
+
 }
+
--- app/src/main/java/com/example/dennismoyseyev/stopwatch/data_model.kt
+++ app/src/main/java/com/example/dennismoyseyev/stopwatch/data_model.kt
@@ -1,13 +1,49 @@
 package com.example.dennismoyseyev.stopwatch
 
+import android.os.SystemClock
+import kotlinx.android.synthetic.main.activity_main_screen.*
 import java.sql.Time
 import java.util.*
 
 
 class data_model()
 {
-   var time : time_class= time_class()
+   private var time : time_class= time_class()
+   var start_time : Long = 0
+   var v: Vector<time_class> = Vector()
+   private var new_total_milliseconds_plus_additional_time: Long=0
+   var total_milliseconds: Long =0
 
 
+    init{
+      v.add(time)
+   }
+
+    internal fun running_time()
+    {
+        time.milliseconds = SystemClock.uptimeMillis() - start_time
+        new_total_milliseconds_plus_additional_time = total_milliseconds +  time.milliseconds
+        time.seconds =(new_total_milliseconds_plus_additional_time / 1000)
+        time.minutes = time.seconds / 60
+        time.seconds = time.seconds % 60
+        time.milliseconds = (new_total_milliseconds_plus_additional_time % 1000)
+    }
+
+    internal fun reset_time()
+    {
+        time.milliseconds=0
+        time.seconds=0
+        time.minutes=0
+        new_total_milliseconds_plus_additional_time=0
+        total_milliseconds=0
+        start_time=0
+
+    }
+
+    //returns the time in a string. To be displayed as the text for the timer.
+    internal fun show_time(): String
+    {
+        return String.format("%2d:%2d:%3d", model.time.minutes, model.time.seconds, model.time.milliseconds)
+    }
 
 }
