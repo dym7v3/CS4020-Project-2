7c56bc17df1292c28faeb71a90f3cbfda4b6a6a0
Dennis Moyseyev <denomoyseyev@gmail.com>
2018-04-03 17:26:01 -0500

    Cleaned up the look of the timer.

--- app/src/main/java/com/example/dennismoyseyev/stopwatch/MainScreen.kt
+++ app/src/main/java/com/example/dennismoyseyev/stopwatch/MainScreen.kt
@@ -17,18 +17,7 @@ var model: data_model= data_model()
 
 class MainScreen : FragmentActivity() {
 
-    private var mHandler = Handler()
-    val laps_adapter= LapsAdapter(model.laps)
-
-    //The function is the one that updates the time with a runnable class.
-    private val update_timer = object : Runnable {
-        override fun run() {
-            model.running_time()
-            Timer.text = model.show_time()
-            //Continues to do this without delay until stop or reset is pressed.
-            mHandler.postDelayed(this, 0)
-        }
-    }
+      private val laps_adapter: data_model.LapsAdapter = data_model.LapsAdapter(this, model.laps)
 
      //Function is called when the activity is created. It will set the content on the screen
      // and bind the buttons to the correct actions.
@@ -39,56 +28,12 @@ class MainScreen : FragmentActivity() {
         list_frag_listview.adapter= laps_adapter
     }
 
-    //Using the example code from:http://javasampleapproach.com/android/kotlin-listview-example-android
-    inner class LapsAdapter (private var lapsList: ArrayList<String>) : BaseAdapter() {
-
-        //The size of the laps list.
-        override fun getCount(): Int {
-            return lapsList.size
-        }
-
-        //Gets the id of at the position.
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        //Adds the content form the arraylist to the view.
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
-
-            val mRow: View?
-            val vh: ViewHolder
-
-            //Checks if the view is null. If it is then it will save the current row as the
-            // convert-view.
-            //I used the view holder to be able to get better scrolling.
-            if (convertView == null) {
-                mRow = layoutInflater.inflate(R.layout.row, parent, false)
-                vh = ViewHolder(mRow)
-                mRow.tag = vh
-            } else {
-                mRow= convertView
-                vh = mRow.tag as ViewHolder
-            }
-            vh.lap_num.text = position.toString()
-            vh.display_time.text =  lapsList[position]
-            return mRow
-        }
-        //Gets the item at the current position that was give the integer.
-        override fun getItem(position: Int): Any {
-            return lapsList[position]
-        }
-    }
-    //Makes the View Holder. Which I read makes better for scrolling.
-    private class ViewHolder(view: View?) {
-        val lap_num: TextView = view?.findViewById(R.id.lap_num) as TextView
-        val display_time: TextView = view?.findViewById(R.id.display_time) as TextView
-    }
 
     //Binds the buttons so that they can actually be used by the user.
     private fun bind_button(){
         start_but.setOnClickListener {
             model.start_time=SystemClock.uptimeMillis()
-            mHandler.postDelayed(update_timer, 0)
+            model.mHandler.postDelayed(update_timer, 0)
         }
 
         //Displays that the stop button was pressed and it will then change the stop to say resume
@@ -96,26 +41,38 @@ class MainScreen : FragmentActivity() {
         stop_but.setOnClickListener {
             Toast.makeText(this, "The Stopwatch was stopped.", Toast.LENGTH_SHORT).show()
             //TODO add the ability to be able to save the time so that you can resume the stopwatch.
-            mHandler.removeCallbacks(update_timer)
+            model.mHandler.removeCallbacks(update_timer)
         }
 
         //Displays the Toast that the reset button was pressed and resets the time back to zero.
         reset_but.setOnClickListener {
             Toast.makeText(this, "The Stopwatch was reset.", Toast.LENGTH_SHORT).show()
-            mHandler.removeCallbacks(update_timer)
+            model.mHandler.removeCallbacks(update_timer)
             model.reset_time()
             Timer.text =model.show_time()
-            model.laps.clear()
-            laps_adapter.notifyDataSetChanged()
+            laps_adapter.notifyDataSetChanged() //Tells us adapter that the data set changed.
         }
 
+        //When the lap button is pressed records the string representation of the time and adds
+        //it to the laps array-list and tells the adapter that the data changed.
         lap_but.setOnClickListener{
-            //Adds the current time as a string to the arraylist of laps.
+            //Adds the current time as a string to the array-list of laps.
             model.laps.add(model.show_time())
             //Tells the adapter the the data set as changed.
             laps_adapter.notifyDataSetChanged()
         }
     }
+
+    //The function is the one that updates the time with a runnable class.
+    private val update_timer = object : Runnable {
+        override fun run() {
+            model.running_time()
+            Timer.text = model.show_time()
+            //Continues to do this without delay until stop or reset is pressed.
+            model.mHandler.postDelayed(this, 0)
+        }
+    }
+
 
 }
 
--- app/src/main/java/com/example/dennismoyseyev/stopwatch/data_model.kt
+++ app/src/main/java/com/example/dennismoyseyev/stopwatch/data_model.kt
@@ -1,18 +1,71 @@
 package com.example.dennismoyseyev.stopwatch
 
+import android.content.Context
+import android.os.Handler
 import android.os.SystemClock
-import java.util.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
 import kotlin.collections.ArrayList
 
 
 class data_model()
 {
-   var time : time_class= time_class()
+   private var time : time_class= time_class()
+   var mHandler = Handler()
    var laps: ArrayList<String> = ArrayList()
    var start_time : Long = 0
    private var new_total_milliseconds_plus_additional_time: Long=0
-   var total_milliseconds: Long =0
+   private var total_milliseconds: Long =0
 
+    //Using the example code from:http://javasampleapproach.com/android/kotlin-listview-example-android
+    internal class LapsAdapter (context: Context, private var lapsList: ArrayList<String>) : BaseAdapter() {
+
+        var mContext= context
+        //The size of the laps list.
+        override fun getCount(): Int {
+            return lapsList.size
+        }
+
+        //Gets the id of at the position.
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        //Adds the content form the arraylist to the view.
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+
+            val mRow: View?
+            val vh: ViewHolder
+
+            //Checks if the view is null. If it is then it will save the current row as the
+            // convert-view.
+            //I used the view holder to be able to get better scrolling.
+            if (convertView == null) {
+                val layoutInflater = LayoutInflater.from(mContext)
+                mRow = layoutInflater.inflate(R.layout.row, parent, false)
+                vh = ViewHolder(mRow)
+                mRow.tag = vh
+            } else {
+                mRow= convertView
+                vh = mRow.tag as ViewHolder
+            }
+            vh.lap_num.text = position.toString()
+            vh.display_time.text =  lapsList[position]
+            return mRow
+        }
+        //Gets the item at the current position that was give the integer.
+        override fun getItem(position: Int): Any {
+            return lapsList[position]
+        }
+    }
+    //Makes the View Holder. Which I read makes better for scrolling.
+    private class ViewHolder(view: View?) {
+        val lap_num: TextView = view?.findViewById(R.id.lap_num) as TextView
+        val display_time: TextView = view?.findViewById(R.id.display_time) as TextView
+    }
 
     internal fun running_time()
     {
@@ -24,6 +77,7 @@ class data_model()
         time.milliseconds = (new_total_milliseconds_plus_additional_time % 1000)
     }
 
+    //Resets the time. Makes everything go back to zero this is including the laps.
     internal fun reset_time()
     {
         time.milliseconds=0
@@ -32,13 +86,43 @@ class data_model()
         new_total_milliseconds_plus_additional_time=0
         total_milliseconds=0
         start_time=0
+        laps.clear() //Removes all the records of the laps.
 
     }
 
     //returns the time in a string. To be displayed as the text for the timer.
     internal fun show_time(): String
     {
-        return String.format("%2d:%2d:%3d", model.time.minutes, model.time.seconds, model.time.milliseconds)
+        val minutes_str: String = time.minutes.toString()
+        val seconds_str: String = time.seconds.toString()
+        val milliseconds_str: String = time.milliseconds.toString()
+        var mTimeString = "" //String that shows the time.
+
+        mTimeString += if(minutes_str.length<2)
+        {
+            ("0$minutes_str:")
+        }
+        else {
+            "$minutes_str:"
+        }
+
+        //Makes sure that the zero is there if the length of the seconds is not 2 digits.
+        mTimeString += if(seconds_str.length<2)
+        {
+            ("0$seconds_str:")
+        }
+        else
+            "$seconds_str:"
+
+        //Makes sure to have the zeros in the right place for the millisecond part of the time.
+        mTimeString += when {
+            milliseconds_str.length==1 -> "00$milliseconds_str"
+            milliseconds_str.length==2 -> "0$milliseconds_str"
+            else -> milliseconds_str
+        }
+
+        //Returns the time with the nice format.
+        return mTimeString
     }
 
 }
